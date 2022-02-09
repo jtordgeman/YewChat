9257a728a5ee89682502c74edef3f66f29a324c0
buserian <buha124a@yahoo.com>
2022-02-09 23:03:07 +0200

    routing

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,14 @@
 #![recursion_limit = "512"]
 
+mod components;
+
 use wasm_bindgen::prelude::*;
 use yew::functional::*;
 use yew::prelude::*;
 use yew_router::prelude::*;
+
+use components::chat::Chat;
+use components::login::Login;
 
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
 // allocator.
@@ -13,3 +18,39 @@ use yew_router::prelude::*;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+#[derive(Debug, Clone, Copy, PartialEq, Routable)]
+pub enum Route {
+    #[at("/")]
+    Login,
+    #[at("/chat")]
+    Chat,
+    #[not_found]
+    #[at("/404")]
+    NotFound,
+}
+
+#[function_component(Main)]
+fn main() -> Html {
+    html! {
+        <BrowserRouter>
+            <div class="flex w-screen h-screen">
+                <Switch<Route> render={Switch::render(switch)}/>
+            </div>
+        </BrowserRouter>
+    }
+}
+
+fn switch(selected_route: &Route) -> Html {
+    match selected_route {
+        Route::Login => html! {<Login />},
+        Route::Chat => html! {<Chat/>},
+        Route::NotFound => html! {<h1>{"404 baby"}</h1>},
+    }
+}
+
+#[wasm_bindgen]
+pub fn run_app() -> Result<(), JsValue> {
+    wasm_logger::init(wasm_logger::Config::default());
+    yew::start_app::<Main>();
+    Ok(())
+}
