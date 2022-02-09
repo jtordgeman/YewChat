e62a8d6bdd3b884e975884f6c59174f0237ae189
buserian <buha124a@yahoo.com>
2022-02-09 22:58:31 +0200

    starter code

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+#![recursion_limit = "512"]
+
+use wasm_bindgen::prelude::*;
+use yew::functional::*;
+use yew::prelude::*;
+use yew_router::prelude::*;
+
+// When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
+// allocator.
+//
+// If you don't want to use `wee_alloc`, you can safely delete this.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
