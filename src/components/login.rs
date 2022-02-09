3c87a64e2d6bf641a98b8feb9c877c0bc9a82263
buserian <buha124a@yahoo.com>
2022-02-09 23:15:16 +0200

    cargo update

--- src/components/login.rs
+++ src/components/login.rs
@@ -28,7 +28,7 @@ pub fn login() -> Html {
 
     html! {
        <div class="bg-gray-800 flex w-screen">
-            <div class="container mx-auto flex flex-col justify-center items-center	">
+            <div class="container mx-auto flex flex-col justify-center items-center">
                 <form class="m-4 flex">
                     <input {oninput} class="rounded-l-lg p-4 border-t mr-0 border-b border-l text-gray-800 border-gray-200 bg-white" placeholder="Username" />
                     <Link<Route> to={Route::Chat}> <button {onclick} disabled={username.len()<1} class="px-8 rounded-r-lg bg-violet-600	  text-white font-bold p-4 uppercase border-violet-600 border-t border-b border-r" >{"Go Chatting!"}</button></Link<Route>>
